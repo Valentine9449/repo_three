d3a49b37faa201b44dce4bc89ca6228454600331
Valik <valik1156@gmail.com>
2022-01-22 18:55:23 +0200

    Поправки

--- second_app.py
+++ second_app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, session, redirect
+from flask import Flask, request, session, redirect, render_template
 
 second_app = Flask(__name__)
 second_app.secret_key = '4a5bc1c65d6f28ef795171cb69144e3658fec03fa2f128fdbf867556cca842c4'
 
+
+# Login
 
 @second_app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -10,15 +12,11 @@ def login():
         session['username'] = request.form['username']
         return redirect('/index')
     elif 'username' not in session:
-        return '''
-               <form method='POST'>
-                 <input name="username">
-                 <input name="password">
-                 <input type="submit">
-             </form>
-        '''
+        return render_template('form.html')
     return redirect('/index')
 
+
+# Index
 
 @second_app.route("/")
 @second_app.route("/index")
@@ -28,16 +26,17 @@ def index():
             session['visits'] = session.get('visits') + 1
         else:
             session['visits'] = 1
-        return f"<h1>Пользователь зашел в систему как {session['username']}</h1>" + \
-               "Total visits: {}".format(session.get('visits'))
+        return render_template('index.html', menu=session)
     return redirect('/login')
+
+# Logout
 
 
 @second_app.route("/logout")
 def logout():
     session.pop('username', None)
     session.pop('visits', None)
-    return 'Пользователь вышел'
+    return render_template('logout.html')
 
 
 if __name__ == '__main__':
